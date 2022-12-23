bbed98e486bbe3372cda7471e06de3441dc238ba
leicester70 <lestercheong70@gmail.com>
2022-12-23 17:13:11 +0800

    error page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ThemeProvider, Typography, createTheme } from '@mui/material';
 import {
   createBrowserRouter,
@@ -5,7 +6,13 @@ import {
 } from "react-router-dom";
 
 // import Pages
-import LandingPage from './pages/landing-page/LandingPage';
+import LandingPage from './pages/LandingPage';
+import Employee from './pages/Employee';
+import Corporate from './pages/Corporate';
+import Doctor from './pages/Doctor';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Error from './pages/Error';
 
 const theme = createTheme({
   palette: {
@@ -25,7 +32,7 @@ const theme = createTheme({
     fontWeightRegular: 500,
     fontWeightMedium: 600,
     fontWeightBold: 600,
-    fontFamily: 'Lato',
+    fontFamily: 'ROboto',
   },
 });
 
@@ -33,22 +40,53 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
+    errorElement: <Error role={`${getRole()}`} />,
     index: true,
   },
   {
     path: "/employee",
-    element: <LandingPage />,
+    element: <Employee />,
+    errorElement: <Error role={`${getRole()}`} />,
+  },
+  {
+    path: "/corporate",
+    element: <Corporate />,
+    errorElement: <Error role={`${getRole()}`} />,
+  },
+  {
+    path: "/doctor",
+    element: <Doctor />,
+    errorElement: <Error role={`${getRole()}`} />,
   },
 ]);
 
 function App() {
+
+  console.log([isNavComponent(), getRole()])
   return (
     <ThemeProvider theme={theme}>
       <Typography>
+        {isNavComponent() ? <NavBar role={`${getRole()}`} /> : null}
         <RouterProvider router={router} />
+        <Footer />
       </Typography>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+function isNavComponent() {
+  let x = window.location.href.split("/")
+  if (x.includes("employee") || x.includes("corporate") || x.includes("doctor")) {
+    return true
+  } else return false
+}
+
+function getRole() {
+  let x = window.location.href.split("/")
+  if (x.includes("employee")) { return "employee" }
+  if (x.includes("corporate")) { return "corporate" }
+  if (x.includes("doctor")) { return "doctor" }
+  return ""
+}
---
+++ src/pages/Error.js
@@ -0,0 +1,25 @@
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { Button, Container, Typography } from "@mui/material";
+import { padding } from "@mui/system";
+
+export default function (props) {
+    return (
+        <Container maxWidth='md' sx={{
+            paddingY: 10,
+            marginY: '10vh',
+            backgroundColor: 'white',
+            boxShadow: 10,
+            alignContent: 'center',
+            textAlign: 'center'
+        }}>
+            <Typography variant="h3" textAlign='center' marginBottom={5}>We're Sorry</Typography>
+            <Player autoplay loop
+                src="https://assets1.lottiefiles.com/packages/lf20_k1rx9jox.json"
+                style={{ width: 350 }}>
+                <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+            </Player>
+            <Typography marginY={5}>We can't find the page you've requested for...</Typography>
+            <Button variant="contained" href={`/${props.role}`}>Return</Button>
+        </Container>
+    );
+}
