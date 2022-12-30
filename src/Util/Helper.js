882b4adf84c6db9ac7965db0b688d3d639a6ef05
leicester70 <lestercheong70@gmail.com>
2022-12-30 16:17:31 +0800

    helper

---
+++ src/Util/Helper.js
@@ -0,0 +1,31 @@
+export function isNavComponent() {
+    let x = window.location.href.split("/")
+    if (x.includes("employee") || x.includes("corporate") || x.includes("doctor")) {
+        return true
+    } else return false
+}
+
+export function getRole() {
+    try {
+        let x = window.location.href.split("/")
+        if (x.includes("employee")) { return "employee" }
+        if (x.includes("corporate")) { return "corporate" }
+        if (x.includes("doctor")) { return "doctor" }
+        let sessionData = window.sessionStorage.getItem("data")
+        return sessionData.role
+    } catch (error) {
+        console.log(`Helper.js, getRole() - Failed\n${error}`)
+    }
+}
+
+export function capitalizeWords(str) {
+    str = String(str).split(" ")
+    return str.map(element => {
+        return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase() + " ";
+    });
+}
+
+export function randomMinMax(min, max) {
+    let x = Math.random() * (max - min) + min;
+    return Math.round(x)
+}
