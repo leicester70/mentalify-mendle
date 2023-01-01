a57f4848bb1def08d03109df842b69adf317285a
leicester70 <lestercheong70@gmail.com>
2023-01-01 21:42:03 +0800

    page for this feature

--- src/pages/EmployeeMakeRequest.js
+++ src/pages/EmployeeMakeRequest.js
@@ -1,7 +1,18 @@
+import { Container } from "@mui/material";
+import ContentContainer from "../components/Common/ContentContainer";
+
 export default function () {
     return (
-        <Container>
-            Employee
+        <Container maxWidth='x1' sx={{
+            backgroundColor: 'rgba(255, 255, 255)',
+            minHeight: 1200,
+        }}>
+            <Container maxWidth='lg' disableGutters sx={{
+                paddingY: 10,
+                bgcolor: 'white',
+            }}>
+
+            </Container>
         </Container>
     );
 }
