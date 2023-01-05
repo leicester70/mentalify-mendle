26ee3047c3b02b2e1edf613af1db56d8d195d271
leicester70 <lestercheong70@gmail.com>
2023-01-05 13:35:27 +0800

    yes make request 🆗 (for now)

--- src/pages/EmployeeMakeRequest.js
+++ src/pages/EmployeeMakeRequest.js
@@ -3,6 +3,7 @@ import { Player, Controls } from "@lottiefiles/react-lottie-player";
 import { Typography, Divider, FormControl, Box, TextField, Button } from "@mui/material";
 import { getEmployeeData } from "../data/DataHelper";
 import Employee from "./Employee";
+import BodyContent from "../components/SimpleComponents/BodyContent";
 
 const issuesCategories = [
     'Oliver Hansen',
@@ -32,36 +33,39 @@ export default function () {
                     <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
                 </Player>
                 <Box mt={4} mb={12}>
-                    <Typography variant="h1" sx={{ my: 2 }}>Make a Request</Typography>
+                    <Typography variant="h2" sx={{ my: 2 }}>Make a Request</Typography>
                     <Typography variant="subtitle1">
                         Describe your situation and let us know your struggles.<br />Mendle will do it's best to connect you and bring you the assistance that
                         best fits your needs.
                     </Typography>
                 </Box>
-                <Divider sx={{ marginY: 4 }}></Divider>
+                <Divider sx={{ marginY: 8 }}></Divider>
 
-                <Grid>
-                    <Grid justifyContent='' container>
-                        <Grid item>
-                            <Player autoplay loop src="https://assets2.lottiefiles.com/packages/lf20_1ef7g2lw.json" style={{ width: "15vw" }}>
-                                <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-                            </Player>
+                <BodyContent
+                    lottieSrc="https://assets2.lottiefiles.com/packages/lf20_1ef7g2lw.json" lottiePlayerSize="220pt"
+                    headingText="Pair up with a Psychologist"
+                    bodyText="Sample Text Body"
+                    buttonText="Receive Assistance"
+                >
+                </BodyContent>
+                <Divider sx={{ marginY: 8 }}></Divider>
+                <BodyContent
+                    lottieSrc="https://assets6.lottiefiles.com/packages/lf20_tulr8tag.json" lottiePlayerSize="200pt"
+                    headingText="Workplace Buddies"
+                    bodyText="Match with Buddies"
+                    buttonText="Find a Buddy"
+                >
+                </BodyContent>
 
-                        </Grid>
-                        <Grid flexGrow={1} item textAlign='left' padding={5}>
-                            <Typography textAlign={"left"} variant="h4" sx={{ my: 2 }}>Make a Request</Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
 
-
-                <FormControl>
+                {/* <FormControl>
                     <TextField sx={{ flexGrow: 1, mb: 2 }} label={'Email Address'} value={String(getEmployeeData().firstName + getEmployeeData().lastName).toLowerCase() + `@${getEmployeeData().company.toLowerCase()}.com`} />
                     <TextField sx={{ flexGrow: 1, mb: 4 }} multiline label={'Message'} placeholder='Let us hear from you!' />
-                    {/* <Divider sx={{ marginY: 3 }}></Divider> */}
                     <Button variant="contained" color="info" sx={{ height: 65, mb: 2 }}>Submit</Button>
                     <Button variant="outlined" href="javascript:history.back()" sx={{ height: 45, fontWeight: 'light' }}>Return to Mendle</Button>
-                </FormControl>
+                </FormControl> */}
+
+
             </Container>
         </Container>
     );
