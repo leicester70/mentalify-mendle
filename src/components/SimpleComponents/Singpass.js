4ab8bdc07b0fe2794df99ed9c40b9f3b6b4e2a98
leicester70 <lestercheong70@gmail.com>
2023-01-06 14:11:59 +0800

    singpass added to SimpleComponent

---
+++ src/components/SimpleComponents/Singpass.js
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+export default function () {
+    return (
+        <Container maxWidth='x1' disableGutters sx={{
+            backgroundColor: 'rgba(255, 255, 255)',
+        }}>
+            <img width="100%" src="/singpass/doc_2023-01-06_13-45-22.png" />
+            {/* <Container maxWidth='lg' sx={{
+                paddingY: 10,
+            }}>
+                SINGPASS<br />
+            </Container> */}
+        </Container>
+    );
+}
