324e5075aaa9b7ceb68ed9797d7849893476f551
leicester70 <lestercheong70@gmail.com>
2023-01-08 20:49:47 +0800

    removed useless

--- src/components/SimpleComponents/PromotionCards.js
+++ src/components/SimpleComponents/PromotionCards.js
@@ -7,25 +7,6 @@ console.log(promotionJson)
 export default function PromotionCards(props) {
     const { ...renderLimit } = props
 
-    // if ((!!renderLimit) && (renderLimit < promotionJson.length() - 1)) {
-    //     promotionJson = promotionJson.slice(0, renderLimit)
-    // }
-
-    // const SinglePromotionComponent = ({ promotion }) => {
-    //     <Grid container item sx={4} >
-    //         <Card sx={{}}>
-    //             <CardMedia sx={{ height: 400 }} image={promotion.imageSrc} />
-    //             <CardContent>
-    //                 <Typography variant="subtitle1" color="text.secondary">{promotion.vendor}</Typography>
-    //                 <Typography gutterBottom variant="h5" component="div">{promotion.headingText}</Typography>
-    //                 <Typography variant="body2" color="text.secondary">{promotion.bodyText}</Typography>
-    //                 <Divider sx={{ marginTop: 6, marginBottom: 6, backgroundColor: 'grey', opacity: '35%' }} />
-    //                 <Typography variant="subtitle1" color="text.secondary">{promotion.startDate} - {promotion.endDate}</Typography>
-    //             </CardContent>
-    //         </Card>
-    //     </Grid>
-    // }
-
     return (
         <Grid container>
             {
