20e81e9294a4eef1953c691d1b207502aec8c006
leicester70 <lestercheong70@gmail.com>
2023-01-09 00:11:39 +0800

    text wrapping for equal card height

--- src/components/SimpleComponents/PromotionCards.js
+++ src/components/SimpleComponents/PromotionCards.js
@@ -2,24 +2,33 @@ import { Card, CardContent, Typography, CardMedia, Divider, Grid } from "@mui/ma
 
 // import Promotion JSON Data
 import promotionJson from "../../data/promotion.json"
+import { margin } from "@mui/system";
 
 export default function PromotionCards(props) {
     const { ...renderLimit } = props
 
+    const textOverflowWrap = ({ vh = '10vh' }) => {
+        return {
+            whiteSpace: 'wrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            minHeight: vh
+        }
+    }
+
     return (
-        <Grid container direction='column' alignContent='left' rowGap={2} paddingTop={2}>
+        <Grid container direction='row' alignContent='left' rowGap={4}>
             {
                 promotionJson.map((promotion) => {
                     return (
-                        <Grid item flexGrow={1}>
+                        <Grid item margin={1} flexGrow={1} md={4} xs={12}>
                             <Card>
                                 <CardMedia sx={{ height: 400 }} image={promotion.imageSrc} />
                                 <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div" sx={textOverflowWrap('2vh')}>{promotion.headingText}</Typography>
                                     <Typography variant="subtitle1" color="text.secondary">{promotion.vendor}</Typography>
-                                    <Typography gutterBottom variant="h5" component="div">{promotion.headingText}</Typography>
-                                    <Typography variant="body2" color="text.secondary">{promotion.bodyText}</Typography>
-                                    <Divider sx={{ marginTop: 6, marginBottom: 6, backgroundColor: 'grey', opacity: '35%' }} />
-                                    <Typography variant="subtitle1" color="text.secondary">{promotion.startDate} - {promotion.endDate}</Typography>
+                                    <Typography variant="subtitle2" color="text.secondary">{promotion.startDate} to {promotion.endDate}</Typography>
+                                    <Typography variant="body2" color="text.secondary" sx={textOverflowWrap}>{promotion.bodyText}</Typography>
                                 </CardContent>
                             </Card>
                         </Grid>
