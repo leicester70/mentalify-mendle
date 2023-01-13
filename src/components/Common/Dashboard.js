4d826c11a647f5e87eccae7ccc12466faa632d82
C. L <82318965+leicester70@users.noreply.github.com>
2023-01-13 12:50:44 +0800

    dashboard stuff pause

--- src/components/Common/Dashboard.js
+++ src/components/Common/Dashboard.js
@@ -1,7 +1,19 @@
-export default function () {
+import { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { getEmployeeData } from "../../data/DataHelper";
+import InboxIcon from '@mui/icons-material/Inbox';
+
+export const Dashboard = () => {
+    const [role, setRole] = useState("")
+    const [messageCount, setMessageCount] = useState(0)
+
+    useEffect(() => {
+    })
+
     return (
-        <div>
-            Enter
-        </div>
+        <Box ml={2} mb={5}>
+            <Typography mb={3} variant="h4" fontWeight='bold'>Hello, {getEmployeeData().firstName}</Typography>
+            <Typography variant="h4">You have {messageCount} messages</Typography>
+        </Box>
     );
 }
