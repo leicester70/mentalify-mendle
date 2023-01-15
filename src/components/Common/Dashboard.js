885d92c3e8155c88e2cc79be558ad0d96f205d0a
C. L <82318965+leicester70@users.noreply.github.com>
2023-01-15 17:24:02 +0800

    improved on dashboard component

--- src/components/Common/Dashboard.js
+++ src/components/Common/Dashboard.js
@@ -1,19 +1,29 @@
 import { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Divider } from "@mui/material";
 import { getEmployeeData } from "../../data/DataHelper";
 import InboxIcon from '@mui/icons-material/Inbox';
+import { Container } from "@mui/system";
 
 export const Dashboard = () => {
     const [role, setRole] = useState("")
-    const [messageCount, setMessageCount] = useState(0)
+    const [notifCount, setNotifCount] = useState(0)
 
     useEffect(() => {
     })
 
     return (
-        <Box ml={2} mb={5}>
-            <Typography mb={3} variant="h4" fontWeight='bold'>Hello, {getEmployeeData().firstName}</Typography>
-            <Typography variant="h4">You have {messageCount} messages</Typography>
-        </Box>
+        <Container mb={0}>
+            <Typography mb={8} variant="h3" fontWeight='bold'>Hello, {getEmployeeData().firstName}</Typography>
+            <Box mb={10}>
+                <Typography variant="h4">Overview</Typography>
+                <Divider sx={{ mt: 1, mb: 3, backgroundColor: 'grey', opacity: '15%' }} />
+                <Typography variant="h5">You have {notifCount} notifications</Typography>
+            </Box>
+            <Box mb={10}>
+                <Typography variant="h4">Overview</Typography>
+                <Divider sx={{ mt: 1, mb: 3, backgroundColor: 'grey', opacity: '15%' }} />
+                <Typography variant="h5">You have {notifCount} notifications</Typography>
+            </Box>
+        </Container>
     );
 }
