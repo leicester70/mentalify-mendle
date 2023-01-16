8051ad96dd6731da0fd53d23df1157b9c1911c64
C. L <82318965+leicester70@users.noreply.github.com>
2023-01-16 10:22:55 +0800

    Doctor's Dashboard to view Fixes #6

--- src/pages/Doctor.js
+++ src/pages/Doctor.js
@@ -1,7 +1,32 @@
+import { Container, Tab, Tabs } from "@mui/material";
+import { useState } from "react";
 export default function () {
+
+    const [clientViewTab, setClientViewTab] = useState("dashboard")
+
     return (
-        <div>
-            doctor
-        </div>
+        <Container maxWidth='x1' sx={{
+            backgroundColor: 'rgba(255, 255, 255)',
+            minHeight: 1000,
+        }}>
+            <Container disableGutters maxWidth='lg' sx={{
+                paddingTop: 10,
+                paddingBottom: 25,
+            }}>
+                <Tabs
+                    centered
+                    indicatorColor="secondary"
+                    textColor="secondary"
+                    // value={roleValue.index}
+                    // onChange={handleRoleTabChange}
+                    aria-label="icon position tabs example"
+                    textAlign='center'
+                >
+                    <Tab label="Dashboard" />
+                    <Tab label="Conversations" />
+                    <Tab label="Send Report" />
+                </Tabs>
+            </Container>
+        </Container>
     );
 }
